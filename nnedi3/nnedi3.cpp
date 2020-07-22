ea095059ea5d60e71bbc492ea1d26e6b77c50b1a
jpsdr <jpsdr.psx@free.fr>
2020-07-22 18:59:17 +0200

    Add SinPowResizeMT on internal resamplers of nnedi3_rpow2.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- nnedi3/nnedi3.cpp
+++ nnedi3/nnedi3.cpp
@@ -1,5 +1,5 @@
 /*
-**                    nnedi3 v0.9.4.56 for Avs+/Avisynth 2.6.x
+**                    nnedi3 v0.9.4.57 for Avs+/Avisynth 2.6.x
 **
 **   Copyright (C) 2010-2011 Kevin Stone
 **
@@ -4362,7 +4362,7 @@ AVSValue __cdecl Create_nnedi3_rpow2(AVSValue args, void* user_data, IScriptEnvi
 				|| (_strnicmp(cshift,"lanczos4resizemt",16)==0) || (_strnicmp(cshift,"blackmanresizemt",16)==0)
 				|| (_strnicmp(cshift,"spline16resizemt",16)==0) || (_strnicmp(cshift,"spline36resizemt",16)==0)
 				|| (_strnicmp(cshift,"spline64resizemt",16)==0) || (_strnicmp(cshift,"gaussresizemt",13)==0)
-				|| (_strnicmp(cshift,"sincresizemt",12)==0));
+				|| (_strnicmp(cshift,"sincresizemt",12)==0) || (_strnicmp(cshift,"sinpowresizemt",14)==0));
 
 			int type = 0;
 			
@@ -4370,7 +4370,7 @@ AVSValue __cdecl Create_nnedi3_rpow2(AVSValue args, void* user_data, IScriptEnvi
 				|| (_strnicmp(cshift,"sincresize",10)==0)) type=1;
 			else
 			{
-				if (_strnicmp(cshift,"gaussresize",11)==0) type=2;
+				if ((_strnicmp(cshift,"gaussresize",11)==0) || (_strnicmp(cshift,"sinpowresize",12)==0)) type=2;
 				else
 				{
 					if (_strnicmp(cshift,"bicubicresize",13)==0) type=3;
